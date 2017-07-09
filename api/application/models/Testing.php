73a07f32094a797e7c10a8c95516d34b6c82e6c1
ZhiJian <low_9871@hotmail.com>
2017-07-09 12:36:31 +0800

    Reports_data v3.0 and testing

--- api/application/models/Testing.php
+++ api/application/models/Testing.php
@@ -1,13 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
 
-class Reports_data extends CI_Model
+class Testing extends CI_Model
 {
-	//array of data taken from nagios log file
 	protected $_nagios_log = array();
-
-	//variable used to store data from nagios log file
-	//adapted from: nagios_data.php
-	//location: ./application/models/nagios_data.php
+	protected $event_data = array();
 
 	protected $properties = array(
 		'time' => '',
@@ -20,10 +16,8 @@ class Reports_data extends CI_Model
 		'messages' => ''
 	);
 
-	//array for properties array
 	protected $properties_array = array();
 
-	//constructor
 	public function __construct()
 	{
 		parent::__construct();
@@ -40,14 +34,6 @@ class Reports_data extends CI_Model
 		}
 
 		fclose($logfile);
-	}
-
-	public function get_events_log($date_selected)
-	{
-		//arrays for each line of data
-		//$event_data[0] is the time
-		//$event_data[1] is the messages
-		$event_data = array();
 
 		//properties_array counter
 		$k = 0;
@@ -58,25 +44,35 @@ class Reports_data extends CI_Model
 			$unixtime = $event_data[0];
 			$messages = $event_data[1];
 			$properties['time'] = unixtime_convert($unixtime);
-
-			if(strcmp($date_selected, $properties['time']) === 0)
-			{
-				$properties['messages'] = $messages;
-				$properties_array[$k] = json_encode($properties);
-				$k++;
-			}
+			$properties['messages'] = $messages;
+			$properties_array[$k] = json_encode($properties);
+			$k++
 		}
-
-		return $properties_array;
 	}
 
-	//function to convert unix timestamp to localtime
-	private function unixtime_convert($unixtime)
+	public function testing_1()
 	{
-		//remove any non-numeric character
-		$new_unixtime = preg_replace('/\D/', '', $unixtime);
+		return '10';
+	}
 
-		return date('M d Y H:i:s', strtotime($new_unixtime));
+	public function testing_2($data_input)
+	{
+		return $data_input;
+	}
+
+	public function testing_3()
+	{
+		return $properties['time'];
+	}
+
+	public function testing_4()
+	{
+		return $properties['messages'];
+	}
+
+	public function testing_5()
+	{
+		return $properties_array[0];
 	}
 }
 
